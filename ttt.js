a4d4380550868e096b73db5395b55db2d0a64ce3
melinabed <melo36049@hotmail.com>
2023-10-13 15:19:57 -1000

    added logic to gameboard

--- ttt.js
+++ ttt.js
@@ -1,15 +1,62 @@
 const startGame = document.querySelectorAll('#diff');
 startGame.forEach((startGame) => {
     startGame.addEventListener('click', () => {
-        console.log("have you started?");
+        Game.start();
     });
 });
 
-function displayController() {
-    const boxElement = document.querySelectorAll('.box');
-    boxElement.forEach((boxElement) => {
-        boxElement.addEventListener('click', () => {
-            console.log('X')
-        })
-    })
+const Gameboard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", ""];
+
+    const setMarker = (index, sign) => {
+        if (index > board.length)
+        return;
+        board[index] = sign;
+    };
+    const getMarker = (index) => {
+        if (index > board.length)
+        return;
+        return board[index];
+    }
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    };
+    return {setMarker, getMarker, reset};
+})();
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").innerHTML,"X"),
+            createPlayer(document.querySelector("#player2").innerHTML, "O")
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard();
+        setMarker();
+        getMarker();
+        reset();
+    };
+    return {
+        start,
+    }
+})();
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
 }
+
+
+
+
+
+
